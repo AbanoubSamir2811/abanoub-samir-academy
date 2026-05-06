58f28fab99b2ff2f89a6b6567bbc8617f42524aa
AbanoubSamir2811 <abanoubsamir2811@gmail.com>
2026-05-06 20:09:35 +0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -5,4 +5,8 @@ import { getRouter } from "./router";
 
 const router = getRouter();
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
